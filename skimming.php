c006e49a831666afdacf5859e71d5a42a5901191
Yusuf Hari P <stoneisrock@gmail.com>
2017-10-24 14:11:15 +0700

    add skimming

--- skimming.php
+++ skimming.php
@@ -1,11 +1,12 @@
 <html>
     <?php
-        $kata = $_POST['kata'];
-        // echo $kata;
-        $link = "http://kateglo.com/api.php?format=json&phrase=".$kata;
-        $data = json_decode(file_get_contents($link), true);
-        echo "kata dasarnya adalah ".$data['kateglo']['root'][0]['root_phrase'];
-        // print_r($data);
+        if(!empty($_POST['kata'])){
+            $kata = $_POST['kata'];
+            $link = "http://kateglo.com/api.php?format=json&phrase=".$kata;
+            $data = json_decode(file_get_contents($link), true);
+            echo "kata dasarnya adalah ".$data['kateglo']['root'][0]['root_phrase'];
+            // print_r($data); 
+        }
     ?>
 
     <form action="/skimming.php" method="post">
@@ -13,4 +14,6 @@
         <input type="text" name="kata"><br>
         <input type="submit" value="Submit">
     </form>
+
+    
 </html>
